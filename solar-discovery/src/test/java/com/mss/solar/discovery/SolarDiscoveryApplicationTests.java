f9a377125469a1db9eae79bc5d959bfb22e87b43
Gowthami Ala <gala@MSSDC.LOCAL>
2018-05-08 12:40:42 +0530

    branch test

--- solar-discovery/src/test/java/com/mss/solar/discovery/SolarDiscoveryApplicationTests.java
+++ solar-discovery/src/test/java/com/mss/solar/discovery/SolarDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mss.solar.config;
+package com.mss.solar.discovery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SolarConfigApplicationTests {
+public class SolarDiscoveryApplicationTests {
 
 	@Test
 	public void contextLoads() {
